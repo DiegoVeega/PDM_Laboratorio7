0b0f215ffff4155eb7dfac11679309abbbfc7859
feeewq3 <16137238uca>
2019-05-17 09:02:53 -0600

    bind

--- app/src/main/java/com/diegoveega/pdm_laboratorio7/activities/MainActivity.kt
+++ app/src/main/java/com/diegoveega/pdm_laboratorio7/activities/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bind()
     }
 
     private fun bind(){
